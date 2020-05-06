0f2567cbc0ee5c74d062492f5aceae0b6e16f5e0
lxq <1824992529@qq.com>
2020-05-07 00:46:43 +0800

    first commit

---
+++ src/test/java/com/lxq/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxq.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
